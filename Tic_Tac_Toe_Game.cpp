9b4b30fdc2e66861f6fad52249cc7c00a45d64e1
Md Kaif <163404336+kaife0@users.noreply.github.com>
2025-03-14 14:19:05 +0530

    Update Tic_Tac_Toe_Game.cpp

--- Tic_Tac_Toe_Game.cpp
+++ Tic_Tac_Toe_Game.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void printBoard(); 
 int checkwin();
 
-char board[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char board[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; 
  
 int main()
 {
